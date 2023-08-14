ca253e99dbccd1d602b0e18cdc8985698dd4c740
JawadErfani01 <jawad.erfani147@gmail.com>
2023-08-14 19:06:11 +0430

    errorHandler added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use("/api/user", userRoutes);
-
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
