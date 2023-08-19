bb46e63bf71fdcbf5a286521d32c0bba59b3adbe
JawadErfani01 <jawad.erfani147@gmail.com>
2023-08-19 16:26:47 +0430

    Redux Toolkit counter app created

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./globalRedux/Features/counter/counterSlice";
+import { RootState } from "./globalRedux/store";
 
 const Home = () => {
-  return <div>Home screen</div>;
+  const dispatch = useDispatch();
+  const count = useSelector((state: RootState) => state.counter.value);
+  return (
+    <div className="text-center flex justify-center items-center flex-col p-8 ">
+      <h1>It is a great counter app</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment By Five
+      </button>
+    </div>
+  );
 };
 
 export default Home;
